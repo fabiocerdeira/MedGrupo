9a9b750e0819e3ec8136fddc96d2580ae4b6f256
Fabio Mahfoud Cerdeira <cerdeira@yahoo.com>
2020-04-30 20:39:17 -0300

    v1

---
+++ script_banco_dados.sql
@@ -0,0 +1,21 @@
+﻿USE [dbMedGrupo]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- DROP TABLE [dbo].[contatos];
+-- GO
+
+CREATE TABLE [dbo].[contatos] (
+    [idContato]      INT           IDENTITY (1, 1) NOT NULL,
+    [nome]           VARCHAR (250) NOT NULL,
+    [dataNascimento] SMALLDATETIME NOT NULL,
+    [sexo]           CHAR (1)      NOT NULL
+);
+GO
+
+
